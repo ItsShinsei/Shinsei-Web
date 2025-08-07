2c45018f574bceeede636b4c2d4052ad16a3e926
ItsShinsei <mnovarahman505@gmail.com>
2025-08-07 15:51:25 +0700

    initial commit

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,20 +1,24 @@
-@extends('layouts.app')
+    @extends('layouts.app')
 
-@section('title', 'Home')
+    @section('title', 'Home')
 
-@section('content')
-   
-
-<section class="h-screen flex flex-col items-center justify-center text-center">
-     <section class="h-screen flex items-center justify-center">
-        <h1 class="text-5xl font-bold">Welcome to My Version</h1>
-    </section>
-
-
-
-    <div>
-        <h1 class="text-6xl font-extrabold mb-4">Hello, I’m Shinsei</h1>
-        <p class="text-xl max-w-xl">I'm from 17 Agustus 1945 vacational highschool. Everyone call me Nova here... but you can call me Shinsei ;)</p>
+    @section('content')
+        <section class="bg-[#FEFEFE] h-screen flex items-center justify-center text-center">
+  <div class="
+    box-content w-full max-w-4xl rounded-lg shadow-lg
+    bg-[#FEFEFE] border border-gray-500 text-[#333] relative
+  ">
+    <!-- Dark top bar -->
+    <div class="
+    w-full h-15 bg-stone-900 rounded-t-lg
+    shadow-3x1
+    "></div>
+    
+    <!-- Content -->
+    <div class="p-8 h-96 flex flex-col items-center justify-start mt-10">
+      <h1 class="flex items-top text-5xl font-bold mb-4">Hello!  <span class="text-red -800">  I’m Shinsei</span></h1>
+      <p class="text-xl max-w-xl">I'm from 17 Agustus 1945 vacational highschool Surabaya.</p>
     </div>
+  </div>
 </section>
-@endsection
+    @endsection
